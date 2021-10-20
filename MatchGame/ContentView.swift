a2cb95d3bc9afda15835f4e29c71f4cd73396868
Phanindra Saggurthi <phani142@gmail.com>
2021-10-20 18:36:02 +0530

    MatchGame first pull request. Basic layout completed.

--- MatchGame/ContentView.swift
+++ MatchGame/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["✈️","🚀","🚁","🏍","🚂","🚈", "🛺","🚙","🛶"]
+    let emojiCount = 4
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        HStack {
+            ForEach(emojis[0..<emojiCount], id:\.self , content: {
+                emoji in
+                CardView(content: emoji)
+            })
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+        ContentView()
+            .preferredColorScheme(.light)
+    }
+}
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape
+                    .fill()
+                    .foregroundColor(.white)
+                shape
+                    .stroke(lineWidth: 3)
+                Text(content)
+                    .font(.largeTitle)
+            } else {
+                shape
+                    .fill()
+                    .foregroundColor(.red)
+            }
+        }
+        .onTapGesture {
+            self.isFaceUp = !isFaceUp
+        }
     }
 }
