e0c5f4a5d4154db0099d7b9ac9396647209f788e
Phanindra Saggurthi <phani142@gmail.com>
2021-10-20 07:37:58 +0530

    Initial Commit

---
+++ MatchGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MatchGame
+//
+//  Created by Phanindra Saggurthi on 20/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MatchGame/MatchGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchGameApp.swift
+//  MatchGame
+//
+//  Created by Phanindra Saggurthi on 20/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MatchGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
